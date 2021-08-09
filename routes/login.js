b3e3069a947b7c33db9c608e6ef1c837fb2ac659
agustin vilarino <lic.avilarino@gmail.com>
2021-08-09 13:30:42 -0300

    login

--- routes/login.js
+++ routes/login.js
@@ -17,7 +17,7 @@ const login = async (req, res) => {
         const [{id, admin}] = logged;
         req.session.user = id;
         req.session.admin = admin;
-        res.redirect('/admin');
+        res.redirect('/');
 
     }
 }
